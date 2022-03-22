1155b479d2ee4608449e110289d2c5c03fa63edf
A R DANISH <helloardanish@gmail.com>
2022-03-23 02:20:29 +0530

    Update Longest_Common_Prefix.py

--- Top-Interview-Questions/Longest_Common_Prefix.py
+++ Top-Interview-Questions/Longest_Common_Prefix.py
@@ -150,4 +150,64 @@ class Solution:
           
           
           
-          
+
+		
+		
+		
+# another solution
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def add_word(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.end = True
+    
+    def longest_prefix(self):
+        res = []
+        cur = self.root
+        while cur:
+            # return when reaches the end of word or when there are more than 1 branches
+            if cur.end or len(cur.children) > 1:
+                return ''.join(res)
+            c = list(cur.children)[0]
+            res.append(c)
+            cur = cur.children[c]
+        return ''.join(res)
+        
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        T = Trie()
+        for s in strs:
+            T.add_word(s)
+        return T.longest_prefix()
+
+
+
+'''
+
+
+Time complexity:
+
+building Trie: O(N*len(s)) where N = len(strs) and s is the longest word in strs
+finding longest prefix: O(M) where M is the length of LCP (which is in the range of 0 and the length of shortest string)
+
+
+'''
